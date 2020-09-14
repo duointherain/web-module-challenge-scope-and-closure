d2f925caafa26d8263ff4698e8985149294270d5
duointherain <duointherain@gmail.com>
2020-09-14 13:35:00 -0700

    first three questions

--- index.js
+++ index.js
@@ -22,36 +22,42 @@ function processFirstItem(stringList, callback) {
 
 
 ///// M V P ///////
+console.log("*** Task 1 ***")
 
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
+ * Counter1 has an extra line of code, return function counter( ) which will remember and up date, and a const value that won't update. One actually doesn't run, but two remembers and updates.
  * 
  * 2. Which of the two uses a closure? How can you tell?
+ * I think one has a closure because there is a function within the higher function. Changing from const to let doesn't make it run though. 
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
+ * Well counter 1 doesn't run so... never. But if it ran and I wanted to call counter in a different part of whatever I was making, then it would be good. 
 */
 
 // counter1 code
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
+  return count++;
   }
 }
-
-const counter1 = counterMaker();
-
+let counter1 = counterMaker;
+console.log(counter1())
+console.log(counter1())
 // counter2 code
 let count = 0;
-
+console.log("*** counter 2**")
 function counter2() {
   return count++;
 }
 
-
+console.log(counter2())
+console.log(counter2())
+console.log(counter2())
+console.log("*** Task 2 ***")
 /* Task 2: inning() 
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
@@ -62,6 +68,10 @@ function inning(/*Code Here*/){
 
 }
 
+
+
+
+console.log("*** Task 3 ***")
 /* Task 3: finalScore()
 
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
@@ -81,7 +91,7 @@ function finalScore(/*code Here*/){
   /*Code Here*/
 
 }
-
+console.log("*** Task 4 ***")
 /* Task 4: 
 
 Create a function called `scoreboard` that accepts the following parameters: 
